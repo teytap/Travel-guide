2b2c8c87fd83e8e8098970b0319f204ba1f44b2d
Mehtap Tataroğlu <teytap@gmail.com>
2023-01-15 22:32:37 +0300

    added images and data, cleaned unnecessary data

---
+++ travel-app/src/Card.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Card() {
+  return <div className="card">card</div>;
+}
---
+++ travel-app/src/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src="/images/Fill 213.png" alt="logo" />
+      <h2>my travel journal.</h2>
+    </nav>
+  );
+}
